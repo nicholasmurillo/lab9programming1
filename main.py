4ce94f18ee273b0fe3600f331965da89a28086ca
Nicholas Murillo <murillona20@gmail.com>
2023-03-22 17:44:01 -0400

    added comments to code

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-def encode(num_string):
-    encoded_string = ""
+def encode(num_string):  # encoding an inputted string
+    encoded_string = ""  # starts empty string
     for i in num_string:
-        temp = int(i) + 3
-        if temp > 10:
-            encoded_string += str(temp - 10)
+        temp = int(i) + 3  # for every number in string, add 3
+        if temp > 10:  # if number surpasses 10, subtract 10 to be within 0-9
+            encoded_string += str(temp - 10)  # add to encoded string
         else:
-            encoded_string += str(temp)
+            encoded_string += str(temp)  # add to encoded string
     return encoded_string
 
 
-def menu():
+def menu():  # menu printed every time in the loop
     print("Menu")
     print("-------------")
     print("1. Encode")
@@ -21,17 +21,17 @@ def menu():
 def main():
     while True:
         menu()
-        option = int(input("Please enter an option: "))
-        if option == 1:
+        option = int(input("Please enter an option: "))  # option given by user
+        if option == 1:  # if option is 1, encode input
             password = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!")
             print("")
-        elif option == 2:
+        elif option == 2:  # if option is 2, display encoded and original password
             print(f"The encoded password is {encode(password)}, and the original password is {password}.")
             print("")
-        elif option == 3:
+        elif option == 3:  # if option is 3, stop loop
             break
-        else:
+        else:  # if option is invalid, continue loop
             print("Invalid option. Please try again.")
 
 
